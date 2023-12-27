0cc5eded520584ed3601f0ff034cfd8d049ac2eb
EAGLEARCHER <bherusinghpanwar43@gmail.com>
2023-12-27 22:38:03 +0530

    request response fixed

--- sasti-naukri-app/src/features/user/userThunk.js
+++ sasti-naukri-app/src/features/user/userThunk.js
@@ -5,6 +5,7 @@ import { logoutUser } from './userSlice';
 export const registerUserThunk = async (url, user, thunkAPI) => {
   try {
     const resp = await customFetch.post(url, user);
+    // console.log(resp.body)
     return resp.data;
   } catch (error) {
     return thunkAPI.rejectWithValue(error.response.data.msg);
