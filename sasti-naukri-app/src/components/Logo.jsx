7b5a307a681d831add9f8e9a18f9a2189f513206
EAGLEARCHER <bherusinghpanwar43@gmail.com>
2024-01-22 14:29:35 +0530

    Update logo image (generated by blackbox.ai )

--- sasti-naukri-app/src/components/Logo.jsx
+++ sasti-naukri-app/src/components/Logo.jsx
@@ -1,5 +1,5 @@
-import logo from "../assets/images/logo.svg";
+import logo from "../assets/images/logo.png";
 
 export default function Logo() {
-  return <img src={logo} alt="jobster logo" className="logo" />;
+  return <img src={logo} alt="jobster logo" className="logo" style={{height:28,width:"auto", mixBlendMode:"multiply"}}/>;
 }
